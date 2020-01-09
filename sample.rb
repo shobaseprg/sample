3b558eeee3df2f918543321127f262a1d61555a3
shobaseprg <pgymlsho19128@gmail.com>
2020-01-09 16:53:13 +0900

    dふぁふぁdふぁ

--- sample.rb
+++ sample.rb
@@ -1,6 +1,10 @@
 def hello
+<<<<<<< HEAD
   p "hello, github."
   goddonaigto
+=======
+  p "hello, github Desktop."
+>>>>>>> parent of cd605b9... Revert "replaced the message in sample."
 end
 
 hello
